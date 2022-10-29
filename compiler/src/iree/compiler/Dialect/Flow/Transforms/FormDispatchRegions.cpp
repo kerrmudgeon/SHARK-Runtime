35fd423a57cb56621a29bd114c111dfba9120fbd
stanley-nod <stanley@nod-labs.com>
2022-10-28 17:45:51 -0700

    [codegen][spirv] Pack/transpose matrix B for better coop mmma

--- compiler/src/iree/compiler/Dialect/Flow/Transforms/FormDispatchRegions.cpp
+++ compiler/src/iree/compiler/Dialect/Flow/Transforms/FormDispatchRegions.cpp
@@ -656,6 +656,12 @@ isFusableWithProducer(OpOperand &operand,
 
   auto consumerLinalgOp = cast<linalg::LinalgOp>(consumer);
   if (consumerLinalgOp.isDpsInput(&operand)) {
+    // TODO: Add some marker on transpose and MatmulOp to indicate mmt.
+    bool fuseTransposeAndMatmul =
+        isa<linalg::MatmulOp>(consumer) && isa<linalg::TransposeOp>(producer);
+    if (fuseTransposeAndMatmul) {
+      return true;
+    }
     // Only fuse on inputs if both ops are generic ops.
     if (!isa<linalg::GenericOp>(consumer) ||
         !isa<linalg::GenericOp>(producer)) {
